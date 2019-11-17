e2e1bd165b7f20d85d259222c41a5faedee736e1
KarloFab <fabijanickarlo1@gmail.com>
2019-11-17 16:07:01 +0100

    Added PetController

---
+++ pet-clinic-web/src/main/java/karlo/springframework/sfgpetclinic/controllers/PetController.java
@@ -0,0 +1,41 @@
+package karlo.springframework.sfgpetclinic.controllers;
+
+import karlo.springframework.sfgpetclinic.model.Owner;
+import karlo.springframework.sfgpetclinic.model.PetType;
+import karlo.springframework.sfgpetclinic.services.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+import java.util.Collection;
+
+@Controller
+@RequestMapping("/owners/{ownerId}")
+public class PetController {
+
+    private static final String VIEWS_PETS_CREATE_OR_UPDATE_FORM = "pets/createOrUpdatePetForm";
+
+    private final PetService petService;
+    private final OwnerService ownerService;
+    private final PetTypeService petTypeService;
+
+    public PetController(PetService petService, OwnerService ownerService, PetTypeService petTypeService) {
+        this.petService = petService;
+        this.ownerService = ownerService;
+        this.petTypeService = petTypeService;
+    }
+
+    @ModelAttribute("types")
+    public Collection<PetType> populatePetTypes() {
+        return petTypeService.findAll();
+    }
+
+    @ModelAttribute("owner")
+    public Owner findOwner(@PathVariable("ownerId") Long ownerId) {
+        return ownerService.findById(ownerId);
+    }
+
+    @InitBinder("owner")
+    public void initOwnerBinder(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+    }
+}
