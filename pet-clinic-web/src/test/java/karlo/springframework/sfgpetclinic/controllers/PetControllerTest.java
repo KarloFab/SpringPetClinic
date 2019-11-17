f7643edc6ad1c08f1262b55664bef6f117f7ba9e
KarloFab <fabijanickarlo1@gmail.com>
2019-11-17 16:11:21 +0100

    Fixed PetType and added constructor for base entity and added initial PetControllerTest

---
+++ pet-clinic-web/src/test/java/karlo/springframework/sfgpetclinic/controllers/PetControllerTest.java
@@ -0,0 +1,53 @@
+package karlo.springframework.sfgpetclinic.controllers;
+
+import karlo.springframework.sfgpetclinic.model.Owner;
+import karlo.springframework.sfgpetclinic.model.PetType;
+import karlo.springframework.sfgpetclinic.services.OwnerService;
+import karlo.springframework.sfgpetclinic.services.PetService;
+import karlo.springframework.sfgpetclinic.services.PetTypeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@ExtendWith(MockitoExtension.class)
+public class PetControllerTest {
+
+    @Mock
+    PetService petService;
+
+    @Mock
+    OwnerService ownerService;
+
+    @Mock
+    PetTypeService petTypeService;
+
+    @InjectMocks
+    PetController petController;
+
+    MockMvc mockMvc;
+
+    Owner owner;
+    Set<PetType> petTypes;
+
+    @BeforeEach
+    void setUp() {
+        owner = Owner.builder().id(1l).build();
+
+        petTypes = new HashSet<>();
+        petTypes.add(PetType.builder().id(1L).name("Dog").build());
+        petTypes.add(PetType.builder().id(2L).name("Cat").build());
+
+        mockMvc = MockMvcBuilders
+                .standaloneSetup(petController)
+                .build();
+    }
+
+
+}
